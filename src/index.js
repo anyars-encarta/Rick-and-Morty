90d26c5c5205173f05a9f772955372fc4f700c12
anyars-encarta <anyarsencarta@gmail.com>
2023-08-09 07:54:46 +0000

    Fetch and display Characters on the UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+// index.js
+//import './index.css';
+import { fetchCharacters } from './modules/loadCharacters.js';
+import { loadCharacters } from './modules/loadCharacters.js';
+import myDateTime from './modules/date-time.js';
 
+
+document.addEventListener('DOMContentLoaded', () => {
+  myDateTime();
+  fetchCharacters ();
+  loadCharacters();
+});
