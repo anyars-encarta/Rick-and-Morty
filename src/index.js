5d1094eefa93ee6a77e4b70e88dc7904a060e2a3
anyars-encarta <anyarsencarta@gmail.com>
2023-08-10 06:46:46 +0000

    Add new like feature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-// index.js
-// import './index.css';
-import { fetchCharacters, loadCharacters } from './modules/loadCharacters.js';
+import {
+  loadCharacters, fetchCharacterDetails, updateModalContent,
+} from './modules/loadCharacters.js';
 import myDateTime from './modules/date-time.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   myDateTime();
-  fetchCharacters();
-  loadCharacters();
+  await loadCharacters();
+
+  // Add event listener to each "Comments" span
+  const commentSpans = document.querySelectorAll('.comment');
+  commentSpans.forEach((commentSpan) => {
+    commentSpan.addEventListener('click', async () => {
+      const characterId = commentSpan.getAttribute('data-id');
+      const character = await fetchCharacterDetails(characterId);
+
+      if (character) {
+        updateModalContent(character);
+      }
+    });
+  });
 });
--- src/modules/loadCharacters.js
+++ src/modules/loadCharacters.js
@@ -5,19 +5,45 @@ export async function fetchCharacters() {
     const data = await response.json();
     return data.results;
   } catch (error) {
-    // console.error('Error fetching characters:', error);
     return [];
+  }
+}
+
+// Add a new function to add a like for a character
+export async function addLike(characterId) {
+  try {
+    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/3rhiucgu7avOD8E9hBq1/likes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        item_id: characterId,
+      }),
+    });
+
+    if (response.ok) {
+      const likeCountElement = document.querySelector(`[data-id="${characterId}"] + .like-count`);
+      if (likeCountElement) {
+        const currentLikes = parseInt(likeCountElement.textContent, 10); // Specify radix parameter
+        likeCountElement.textContent = `${currentLikes + 1} Like${currentLikes === 0 ? '' : 's'}`;
+      }
+    } else {
+      throw new Error('Failed to add like');
+    }
+  } catch (error) {
+    throw new Error('Error adding like');
   }
 }
 
 // Load characters to UI
 export async function loadCharacters() {
   const charactersContainer = document.querySelector('.all-characters');
-  const characterCountElement = document.getElementById('character-count');
+  // const characterCountElement = document.getElementById('character-count');
   const characters = await fetchCharacters();
 
   // Display Character counts
-  characterCountElement.textContent = `Characters(${characters.length})`;
+  // characterCountElement.textContent = `Characters(${characters.length})`;
 
   characters.forEach((character) => {
     const characterElement = document.createElement('div');
@@ -27,16 +53,94 @@ export async function loadCharacters() {
         <div class="below-image">
           <p class="character-name">${character.name}</p>
           <div class="likes">
-            <i class="fa-regular fa-heart"></i>
-            <p>Likes</p>
+            <i class="fa-regular fa-heart" data-id="${character.id}"></i>
+            <p class="like-count">0 Likes</p>
           </div>
         </div>
         <div class="reactions">
-          <span class="comment">Comments</span>
+          <span class="comment" data-id="${character.id}" data-bs-toggle="modal" data-bs-target="#modal-container">Comments</span>
           <span class="reservation">Reservations</span>
         </div>
       `;
 
     charactersContainer.appendChild(characterElement);
   });
+
+  // Add event listener to each "Likes" icon
+  const likeIcons = document.querySelectorAll('.fa-heart');
+  likeIcons.forEach((likeIcon) => {
+    likeIcon.addEventListener('click', async () => {
+      const characterId = likeIcon.getAttribute('data-id');
+      await addLike(characterId);
+    });
+  });
+
+  return characters; // Return the characters array
+}
+
+// Function to fetch additional character details
+export async function fetchCharacterDetails(characterId) {
+  try {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${characterId}`);
+    const data = await response.json();
+    return {
+      image: data.image,
+      name: data.name,
+      status: data.status,
+      species: data.species,
+      gender: data.gender,
+      origin: data.origin,
+      comments: ['Comment 1', 'Comment 2', 'Comment 3'], // Replace with actual comments data
+    };
+  } catch (error) {
+    return null;
+  }
+}
+
+// Function to update modal content with character details
+export function updateModalContent(character) {
+  const modalBody = document.querySelector('.modal-body');
+  if (!modalBody) {
+    throw new Error('Modal body not found');
+    // return;
+  }
+
+  const characterContainer = modalBody.querySelector('.character-content');
+  if (!characterContainer) {
+    throw new Error('Character container not found');
+    // return;
+  }
+
+  const features1 = modalBody.querySelector('.features-1');
+  if (!features1) {
+    throw new Error('Features 1 container not found');
+    // return;
+  }
+
+  const features2 = modalBody.querySelector('.features-2');
+  if (!features2) {
+    throw new Error('Features 2 container not found');
+    // return;
+  }
+
+  const commentsList = modalBody.querySelector('ul'); // Directly target the <ul> element
+  if (!commentsList) {
+    throw new Error('Comments list not found');
+    // return;
+  }
+
+  characterContainer.querySelector('img').src = character.image;
+  characterContainer.querySelector('p').textContent = character.name;
+
+  features1.innerHTML = `
+    <p><b>Status:</b> ${character.status}</p>
+    <p><b>Species:</b> ${character.species}</p>
+  `;
+
+  features2.innerHTML = `
+    <p><b>Gender:</b> ${character.gender}</p>
+    <p><b>Origin:</b> ${character.origin.name}</p>
+  `;
+
+  commentsList.innerHTML = character.comments.map((comment) => `<li>${comment}</li>`).join('');
 }
