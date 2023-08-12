ab6c2be4de095a57fe1c3e23a7e9a67fad820690
unknown <kipkemoiedgah@gmail.com>
2023-08-12 15:45:06 +0300

    add commit js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import {
-  loadCharacters, fetchCharacterDetails, updateModalContent,
+  addLike,
+  loadCharacters,
+  fetchCharacterDetails,
+  updateModalContent,
+  fetchComments,
 } from './modules/loadCharacters.js';
 import myDateTime from './modules/date-time.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
   myDateTime();
-  await loadCharacters();
 
-  // Add event listener to each "Comments" span
+  try {
+    await loadCharacters();
+
+    const charactersContainer = document.querySelector('.all-characters');
+    charactersContainer.addEventListener('click', async (event) => {
+      if (event.target.classList.contains('fa-heart')) {
+        event.stopImmediatePropagation();
+        const characterId = event.target.getAttribute('data-id');
+        event.target.classList.add('disabled');
+        await addLike(characterId);
+        event.target.classList.remove('disabled');
+      }
+    });
+  } catch (error) {
+    throw new Error('Error:', error);
+  }
+
   const commentSpans = document.querySelectorAll('.comment');
   commentSpans.forEach((commentSpan) => {
     commentSpan.addEventListener('click', async () => {
@@ -15,6 +34,8 @@ document.addEventListener('DOMContentLoaded', async () => {
       const character = await fetchCharacterDetails(characterId);
 
       if (character) {
+        const comments = await fetchComments('bteUMr9caK7j5e9t91ze', characterId);
+        character.comments = comments;
         updateModalContent(character);
       }
     });
